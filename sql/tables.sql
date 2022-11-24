6ee3896f5ba57757b2d0c7abce52c09e8b6e78a5
Yonela-Johannes <johannesyonela@gmail.com>
2022-11-24 17:13:52 +0200

    tests

--- sql/tables.sql
+++ sql/tables.sql
@@ -33,3 +33,9 @@ CREATE TABLE IF NOT EXISTS bookings(
     FOREIGN KEY (treatment_id) REFERENCES treatment(ID) ON DELETE CASCADE,
     FOREIGN KEY (stylist_id) REFERENCES stylist(ID) ON DELETE CASCADE
 );
+
+-- Add insert scripts here
+insert into treatment (type, price, code) values ('Pedicure', 175, 'ped');
+insert into treatment (type, price, code) values ('Manicure', 215, 'man');
+insert into treatment (type, price, code) values ('Make up', 185, 'mak');
+insert into treatment (type, price, code) values ('Brows & Lashes', 240, 'las');
